68a8b6e21e7370592db06cd7057978f17a07d168
zotakk4o <zotak3105@gmail.com>
2019-01-20 21:21:43 +0200

    Fix sentences with even score ranking. Stem words for better accuracy.

--- summarizer.py
+++ summarizer.py
@@ -1,39 +1,38 @@
+from nltk.stem import PorterStemmer
 from nltk.tokenize import sent_tokenize, word_tokenize
 from nltk.corpus import stopwords
 from string import punctuation
 import os
+import operator
 
 
-
-class Summarizer():
+class Summarizer:
 
     def __init__(self):
+        self.ps = PorterStemmer()
         self.scored_words = {}
         self.scored_sentences = {}
         self.sentences = []
         self.words = []
-        self.stop_words = stopwords.words("english") + list(punctuation)
+        self.stop_words = stopwords.words("english") + list(punctuation) + ["'s", "'ve", "'ll", "'d", "'t", "'m"]
 
     def process_text(self, file_name):
         with open(file_name, 'r') as f:
             lines = f.read().replace(os.linesep, " ")
             self.sentences = sent_tokenize(lines)
-            self.words = [word for word in word_tokenize(lines) if word not in self.stop_words]
+            self.words = [self.format_word(word) for word in word_tokenize(lines) if
+                          self.format_word(word) not in self.stop_words]
 
-    def summarize(self, file_read, file_write, sentences=0):
+    def summarize(self, file_read, file_write, percentage=60):
         self.process_text(file_read)
-        self.create_frequency_dict()
 
-        if sentences == 0:
-            sentences = int(len(self.sentences) * 6 / 10)
-        elif sentences > len(self.sentences):
-            sentences = len(self.sentences)
-
-        temp_scored_sents = sorted(self.scored_sentences, reverse=True)
         output = []
+        length_of_summary = int(len(self.sentences) * percentage / 100)
 
-        for score in temp_scored_sents[:sentences]:
-            output.append(self.scored_sentences[score])
+        for index, score in self.create_frequency_dict()[:length_of_summary]:
+            output.append(index)
+
+        output = sorted(output)
 
         with open(file_write, "w") as f:
             f.write(os.linesep.join([self.sentences[i] for i in sorted(output)]))
@@ -48,14 +47,18 @@ class Summarizer():
             else:
                 self.scored_words[word] = 1
 
-        print(self.scored_words)
-
         for i, sentence in enumerate(self.sentences):
-            self.scored_sentences[sum([self.scored_words[word] for word in word_tokenize(sentence) if word in self.words])] = i
+            self.scored_sentences[i] = sum(
+                [self.scored_words[self.format_word(word)] for word in word_tokenize(sentence) if
+                 self.format_word(word) in self.words])
+
+        return sorted(self.scored_sentences.items(), key=operator.itemgetter(1), reverse=True)
+
+    def format_word(self, word):
+        return self.ps.stem(word.lower())
 
 
 summarizer = Summarizer()
-summarizer.summarize("summaries/literature.txt", "summaries/literature-summary.txt")
-summarizer.summarize("summaries/science.txt", "summaries/science-summary.txt")
-summarizer.summarize("summaries/sports.txt", "summaries/sports-summary.txt")
-
+summarizer.summarize("summaries/literature.txt", "summaries/literature-summary.txt", 50)
+summarizer.summarize("summaries/science.txt", "summaries/science-summary.txt", 50)
+summarizer.summarize("summaries/sports.txt", "summaries/sports-summary.txt", 50)
