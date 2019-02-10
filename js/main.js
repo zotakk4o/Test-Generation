ccacce34ab31328c52530aaaff40fbef65974969
zotakk4o <zotak3105@gmail.com>
2019-02-10 18:34:44 +0200

    Update interface

--- js/main.js
+++ js/main.js
@@ -4,6 +4,8 @@ function loadFunctions() {
     $(document).ready(function () {
         handleRangeInputs();
         handleTextAreaAppearance();
+        handleFileUpload();
+        handleFormSubmit();
     });
 }
 
@@ -15,14 +17,14 @@ function handleRangeInputs() {
         for (let index = 0; index < spans.length; index++) {
             if (index === 0 && spans.length === 1) {
                 let span = $(spans[0]);
-                span.text(parseInt(slider.val()));
+                span.text(parseInt(slider.val()) + "%");
             }
             if (index === 0 && spans.length > 1) {
                 let span = $(spans[0]);
-                span.text(100 - parseInt(slider.val()));
+                span.text(100 - parseInt(slider.val()) + "%");
             } else if (index === 1) {
                 let span = $(spans[1]);
-                span.text(parseInt(slider.val()));
+                span.text(parseInt(slider.val()) + "%");
             }
         }
     })
@@ -30,11 +32,36 @@ function handleRangeInputs() {
 
 function handleTextAreaAppearance() {
     $('div.interactive label[for="raw-text"]').on('click', function (e) {
-        let textarea = $(e.target).siblings('textarea');
-        if(textarea.hasClass("hidden")) {
-            textarea.removeClass("hidden");
+        let item = $(e.target).siblings('div');
+        let interactiveSibling = $(e.target).parent().siblings('div.interactive');
+        if (item.hasClass("hidden")) {
+            item.removeClass("hidden");
+            interactiveSibling.addClass("hidden");
         } else {
-            textarea.addClass("hidden");
+            item.addClass("hidden");
+            interactiveSibling.removeClass("hidden");
         }
+    })
+}
+
+function handleFileUpload() {
+    $('#file-upload').on('change', function (e) {
+        let label = $(e.target).siblings('label');
+        let interactiveSibling = $(e.target).parent().siblings('div.interactive');
+
+        if (this.files && this.files.length > 0) {
+            label.html("<i class=\"fas fa-upload\"></i>" + this.files[0].name);
+            interactiveSibling.addClass("hidden");
+        } else {
+            label.html("<i class=\"fas fa-upload\"></i>Upload a file");
+            interactiveSibling.removeClass("hidden");
+        }
+    })
+}
+
+function handleFormSubmit() {
+    $('#generate').on('click', function (e) {
+        let btn = $(e.target);
+        console.log(this.files);
     })
 }
