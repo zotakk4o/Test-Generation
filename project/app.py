34dec8cd2b1e8fad30374f27177ee01b590550d5
zotakk4o <zotak3105@gmail.com>
2019-02-23 12:56:51 +0200

    Add Ajax and loader gif to interface

--- project/app.py
+++ project/app.py
@@ -1,15 +1,30 @@
 from __future__ import print_function
-from flask import Flask, render_template, make_response
+from flask import Flask, render_template, make_response, jsonify, request
 
 app = Flask(__name__)
 app.secret_key = 's3cr3t'
 app.debug = True
+
 
 @app.route('/', methods=['GET'])
 def index():
     title = 'Create the input'
     return render_template('index.html',
                            title=title)
+
+
+@app.route('/summarize', methods=['POST'])
+def summarize():
+    if request.files:
+        file = request.files['file-upload'].read()
+        text = str(file.decode("utf-8"))
+        print(text)
+    elif request.form:
+        for field_name, value in request.form.items():
+            print(field_name)
+            print(value)
+    return make_response(jsonify({'asd': 'asd'}), 200)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
--- project/static/js/main.js
+++ project/static/js/main.js
@@ -6,6 +6,8 @@ function loadFunctions() {
         handleTextAreaAppearance();
         handleFileUpload();
         handleFormSubmit();
+        handleUndoButton();
+        handleAjaxLoader();
     });
 }
 
@@ -40,11 +42,14 @@ function handleTextAreaAppearance() {
 function handleFileUpload() {
     $('#file-upload').on('change', function (e) {
         let label = $(e.target).siblings('label');
+        let undo = $('i#undo-selection');
         let interactiveSibling = $(e.target).parent().siblings('div.interactive');
 
         if (this.files && this.files.length > 0) {
             label.html("<i class=\"fas fa-upload\"></i>" + this.files[0].name);
             interactiveSibling.addClass("hidden");
+            undo.removeClass('hidden');
+            clearErrors();
         } else {
             label.html("<i class=\"fas fa-upload\"></i>Upload a file");
             interactiveSibling.removeClass("hidden");
@@ -57,6 +62,57 @@ function handleFormSubmit() {
         let btn = $(e.target);
         let file = $('#file-upload')[0].files[0];
         let rawText = $('#raw-text').val();
-        console.log(rawText);
-    })
+        if (file) {
+            clearErrors();
+        } else if ($.trim(rawText).length) {
+            clearErrors();
+        } else {
+            btn.siblings('.error').removeClass('hidden');
+        }
+
+        if($('input[type=checkbox]:checked').length < 1) {
+            $('div.error-checkbox').removeClass('hidden');
+        } else {
+            $('div.error-checkbox').addClass('hidden');
+        }
+
+        if($('div.error:not(.hidden)').length === 0) {
+            let form_data = new FormData($('form#test-generation-form')[0]);
+            console.log(form_data);
+            $.ajax({
+                method: "POST",
+                url: '/summarize',
+                data: form_data,
+                contentType: false,
+                cache: false,
+                processData: false,
+                success: function (data) {
+                    console.log(data);
+                }
+            });
+        }
+    });
+
 }
+
+function handleUndoButton() {
+    $('i#undo-selection').click(function (e) {
+        $(e.target).addClass('hidden');
+        $('input#file-upload').val('');
+        $('div#file-upload-container label').html("<i class=\"fas fa-upload\"></i>Upload a file");
+        $('div#raw-text-container').removeClass('hidden');
+    });
+}
+
+function handleAjaxLoader() {
+    $(document).ajaxStart(function(){
+        $('div#loader').show();
+    }).ajaxStop(function(){
+        $('div#loader').hide();
+    });
+}
+
+function clearErrors() {
+    $('form div.error').addClass('hidden');
+}
+
